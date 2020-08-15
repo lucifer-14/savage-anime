c969735393b966f5cba7823f6fc0521b2ad0f4a7
lucifer-14 <69714078+lucifer-14@users.noreply.github.com>
2020-08-15 20:45:45 +0630

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -29,7 +29,7 @@
     <?php if(isset($_SESSION['userId'])) : ?>
     <div class="user-panel mt-3 pb-3 mb-3 d-flex">
       <div class="image">
-        <img src="<?php echo $_SESSION['photo'] ?? "" ?>" class="img-circle elevation-2" alt="User Image" style="height:100% !important; background-position: center; background-size: auto;">
+        <img src="<?php echo $_SESSION['photo'] ?? "" ?>" class="img-circle elevation-2" alt="User Image" style="width: 34px !important">
       </div>
       <div class="info">
         <a href="setting.php" class="d-block"><?php echo $_SESSION['username'] ?></a>
