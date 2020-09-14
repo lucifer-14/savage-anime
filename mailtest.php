12260fec52e667858ad45d243544813f7a42a860
lucifer-14 <69714078+lucifer-14@users.noreply.github.com>
2020-09-15 01:33:39 +0630

    Update mailtest.php

--- mailtest.php
+++ mailtest.php
@@ -16,7 +16,7 @@ $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
 $email->addContent(
     "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
 );
-$sendgrid = new \SendGrid(getenv('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'));
+$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
 try {
     $response = $sendgrid->send($email);
     print $response->statusCode() . "\n";
